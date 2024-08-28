2f8f467e075ef577f83d2c1af8916d86e0f78c4c
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-28 11:59:41 -0700

    a

--- executing.c
+++ executing.c
@@ -1,42 +1,50 @@
-#include "myshell.h"
+#include"myshell.h"
 /**
- * execution - executing a single command
- * @str: command to execute
- * Return: Null, zero
+ * execution - Executes a command parsed from input.
+ * @str: The command to execute.
+ *
+* Return: Nothing.
  */
-
 void execution(char *str)
 {
-    pid_t childprocess = fork();
-    extern char **environ; /** Access to the environment **/
+	pid_t childprocess;
+	char *args[MAX_ARGS]; /* Array to hold command and NULL */
+	char *token;
+	char *delim = " ";
 
-    /** Handling exit command **/
-    if (strcmp(str, "exit") == 0)
-    {
-        exit(EXIT_SUCCESS);
-    }
+	if (strcmp(str, "exit") == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
 
-    if (childprocess == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (childprocess == 0)
-    {
-        /** Dynamically allocate the args array **/
-        char *args[2];
-        args[0] = str;
-        args[1] = NULL; /** Command without arguments **/
+	token = strtok(str, delim);
 
-        if (execve(args[0], args, environ) == -1)
-        {
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        waitpid(childprocess, NULL, 0);
-    }
+	if (token != NULL)
+	{
+		args[0] = token; /* Command to execute */
+		args[1] = NULL;
+		childprocess = fork();
+
+		if (childprocess == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (childprocess == 0)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(childprocess, NULL, 0);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "No command entered\n");
+	}
 }
-
--- myshell.h
+++ myshell.h
@@ -1,11 +1,13 @@
 #ifndef MYSHELL_H
 #define MYSHELL_H
+#define MAX_ARGS 2
 
 #include<stdio.h>
 #include<string.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<sys/wait.h>
+
 void prnt(const char *str);
 void promptt(void);
 void execution(char *str);
