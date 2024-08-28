97c8c0317da3d7d43dd9ef8a920c270effc87931
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-28 10:44:08 -0700

    a

--- executing.c
+++ executing.c
@@ -1,49 +1,42 @@
 #include "myshell.h"
 /**
- * execution - executing process
- * @str: commands/arguments
+ * execution - executing a single command
+ * @str: command to execute
  * Return: Null, zero
  */
 
 void execution(char *str)
 {
-	pid_t childprocess = fork();
-/**Handling exit command**/
-	if (strcmp(str, "exit") == 0)
-	{
-		exit(EXIT_SUCCESS);
-	}
+    pid_t childprocess = fork();
+    extern char **environ; /** Access to the environment **/
 
-	if (childprocess == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (childprocess == 0)
-	{ /**parsing**/
-		char *args[300]; /**storing memory for command**/
-		int argcount = 0; /**counting no. of args in command**/
-		char *token; /*for tokenization**/
-		char *delim = " "; /**tokenize with space**/
-		char *envp[] = {NULL}; /**environment variables (if needed)**/
+    /** Handling exit command **/
+    if (strcmp(str, "exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
 
-		token = strtok(str, delim);
+    if (childprocess == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (childprocess == 0)
+    {
+        /** Dynamically allocate the args array **/
+        char *args[2];
+        args[0] = str;
+        args[1] = NULL; /** Command without arguments **/
 
-		while (token != NULL && argcount < 299)
-		{
-			args[argcount++] = token;
-			token = strtok(NULL, delim);
-		}
-		args[argcount] = NULL;
-
-		if (execve(args[0], args, envp) == -1)
-		{
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		waitpid(childprocess, NULL, 0);
-	}
+        if (execve(args[0], args, environ) == -1)
+        {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        waitpid(childprocess, NULL, 0);
+    }
 }
+
